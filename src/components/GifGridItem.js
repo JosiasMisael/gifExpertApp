46cd91b641ee635573e2eb4e0ce0fc062a9fa865
Josias <51934280+JosiasMisael@users.noreply.github.com>
2021-04-24 20:30:16 -0600

    docs agregados

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const GifGridItem = ({title, url}) => {
     return (
         <div className="carta animate__animated animate__bounceIn">
-            <img src={url} alt={title}></img>
+            <img width="250px;" src={url} alt={title}></img>
             <p>{title}</p>
         </div>
     )
